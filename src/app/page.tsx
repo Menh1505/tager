d5eafcb1465522a84e048ce3ca0f98ee3c460dee
Menhythien <dinhthienmenh1505@gmail.com>
2025-06-29 23:37:03 +0700

    feat(ui): avatar button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 export default function Home() {
@@ -17,8 +18,7 @@ export default function Home() {
 
   return (
     <div>
-      Home
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
