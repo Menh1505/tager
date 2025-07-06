a6a3310ecd1cfd901d47bae90e931231b3a7777a
Menhythien <dinhthienmenh1505@gmail.com>
2025-07-06 09:52:52 +0700

    feat: task page

---
+++ src/app/(standalone)/tasks/page.tsx
@@ -0,0 +1,9 @@
+import { TaskList } from "@/components/taskList";
+
+export default function TasksPage() {
+  return (
+    <div className="container mx-auto px-4 py-6">
+      <TaskList />
+    </div>
+  );
+}
